265d5ab500c757a5ffc5437fd2f7270c8119e8be
codeblue0320-dev <code.blue0320@gmail.com>
2026-07-12 09:30:02 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='wine250-v4';
+const ASSETS=['./','./index.html','./style.css','./data.js','./app.js','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin===location.origin){
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  } // 외부(위키피디아 사진)는 캐시하지 않고 네트워크 직행
+});
